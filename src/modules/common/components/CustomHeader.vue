4217e7df54813ec7f54a624a776bac5baa40bfef
Piojoso <work.leonel.bongiovanni@outlook.com.ar>
2026-08-05 22:37:11 -0300

    Installed input group from shadcn-vue

--- src/modules/common/components/CustomHeader.vue
+++ src/modules/common/components/CustomHeader.vue
@@ -1,3 +1,30 @@
-<template></template>
+<template>
+  <div class="flex flex-row py-3 px-5 gap-5">
+    <h1 class="flex-1 text-2xl font-light">LibrosLibres.com</h1>
 
-<script setup lang="ts"></script>
+    <div class="flex-5">
+      <InputGroup class="w-[500px] m-auto">
+        <InputGroupInput placeholder="Search..." />
+        <InputGroupAddon>
+          <SearchIcon />
+        </InputGroupAddon>
+      </InputGroup>
+    </div>
+
+    <div class="flex-1">
+      <Button :variant="'ghost'"> Iniciar Sesión </Button>
+
+      <Button variant="ghost"> Registrarse </Button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group";
+import { SearchIcon } from "@lucide/vue";
+</script>
